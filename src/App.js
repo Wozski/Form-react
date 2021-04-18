141eceeebf89ed2a68c39e10b5fb4c3dc269e21b
Wozski <banawozski@gmail.com>
2021-04-18 13:10:09 +0800

    change

--- src/App.js
+++ src/App.js
@@ -1,66 +1,61 @@
-import styled from "styled-components";
-import { useState, useEffect } from "react";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import LoginPage from "./Pages/LoginPage";
-import HomePage from "./Pages/HomePage";
-import AddPage from "./Pages/AddPage";
-import AboutPage from "./Pages/AboutPage";
-import EdditPage from "./Pages/EdditPage";
-import Header from "./Header";
-import PostPages from "./Pages/ShowPost";
-import RegisterPage from "./Pages/RegisterPage";
-import { AuthContext } from "./contexts";
-import { getMe } from "./WebAPI";
-const Root = styled.div``;
-function App() {
-  const [isLoding, setIsLoding] = useState(true);
-  console.log(isLoding);
-  const [user, setUser] = useState(null);
-  useEffect(() => {
-    // 有 token 才打 api
-    getMe().then((response) => {
-      if (response.ok) {
-        // 重新整理後，不會改變登入狀況
-        setUser(response.data);
-        setIsLoding(false);
-      }
+import "./App.css";
+import { useState } from "react";
+import calculateWinner from "./utils";
+export default function App() {
+  const [board, setBoard] = useState({
+    squares: Array(19 * 19).fill(null),
+    xIsNext: true,
+    currentX: null,
+    currentY: null,
+  });
+  const block = Array(19 * 19).fill(null);
+  // board.squares 子的顏色，一開始黑色為 true
+  const renderSquare = (i) => {
+    return <Square value={board.squares[i]} onClick={() => handleClick(i)} />;
+  };
+  const handleClick = (i) => {
+    const { squares, currentX, currentY } = board;
+    //防止重複下棋。
+    if (calculateWinner(squares, currentX, currentY) || squares[i]) {
+      return;
+    }
+    // squares[i] = 子的值，例如 B or W
+    squares[i] = board.xIsNext ? "B" : "W";
+    setBoard({
+      squares: squares,
+      xIsNext: !board.xIsNext,
+      currentX: i % 19,
+      currentY: Math.floor(i / 19),
     });
-  }, []);
+  };
+  const Square = (props) => {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value && (
+          <div className={props.value === "B" ? "black" : "white"}></div>
+        )}
+      </button>
+    );
+  };
+  const winner = calculateWinner(board.squares, board.currentX, board.currentY);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player:" + (board.xIsNext ? "B" : "W");
+  }
+  //item = null, index = number
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
-      <Root>
-        <Router>
-          <Header />
-          <Switch>
-            <Route exact path="/">
-              <HomePage />
-            </Route>
-            <Route exact path="/posts/:page">
-              <HomePage />
-            </Route>
-            <Route exact path="/about">
-              <AboutPage />
-            </Route>
-            <Route exact path="/login">
-              <LoginPage />
-            </Route>
-            <Route exact path="/register">
-              <RegisterPage />
-            </Route>
-            <Route exact path="/new-post">
-              <AddPage />
-            </Route>
-            <Route exact path="/eddit">
-              <EdditPage />
-            </Route>
-            <Route exact path="/post/:id">
-              <PostPages />
-            </Route>
-          </Switch>
-        </Router>
-      </Root>
-    </AuthContext.Provider>
+    <div className="game-board">
+      <div className="status">{status}</div>
+      <div className="word">人生如棋，落子無悔</div>
+      <div className="board">
+        <div className="board_block">
+          {block.map((item, index) => (
+            <div className="block">{renderSquare(index)}</div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
-
-export default App;
--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,53 @@
-const TOKEN_NAME = "token";
+export default function calculateWinner(squares, x, y) {
+  if (x === null || y === null) {
+    return null;
+  }
+  // 轉化成二維陣列
+  let boardCoordinates = [];
+  for (let i = 0; i < 19; i += 1) {
+    const start = i * 19;
+    const end = start + 19;
+    // 檢查每一行的值，從 y 開始，之後 x
+    boardCoordinates.push(squares.slice(start, end));
+  }
+  // 取得當前顏色,並且會檢查上一步的棋顏色
+  const currentPieces = boardCoordinates[y][x];
+  function checkLine(currentX, currentY, directionX, directionY) {
+    let nextX = currentX;
+    let nextY = currentY;
+    let lineLength = 0;
 
-export const setAuthToken = (token) => {
-  localStorage.setItem(TOKEN_NAME, token);
-};
-
-export const getAuthToken = () => {
-  return localStorage.getItem(TOKEN_NAME);
-};
+    do {
+      nextX += directionX;
+      nextY += directionY;
+      // 如果沒有條件 >= 0; < 19 條件，第一行和最後一行皆不可點擊，會出現 error，作用是檢查周圍是否有相同棋子。
+      if (
+        nextX >= 0 &&
+        nextX < 19 &&
+        nextY >= 0 &&
+        nextY < 19 &&
+        boardCoordinates[nextY][nextX] === currentPieces
+      ) {
+        lineLength += 1;
+      } else {
+        break;
+      }
+    } while (lineLength);
+    return lineLength;
+  }
+  // 計算不含自己有沒有超過四個子，自身不算，如果有連線，總數加一，最後到四即是五子連線。
+  if (
+    // 檢查前後
+    checkLine(x, y, 1, 0) + checkLine(x, y, -1, 0) >= 4 ||
+    // 檢查上下
+    checkLine(x, y, 0, 1) + checkLine(x, y, 0, -1) >= 4 ||
+    // 檢查右上左下斜角
+    checkLine(x, y, 1, 1) + checkLine(x, y, -1, -1) >= 4 ||
+    // 檢查左上右下斜角
+    checkLine(x, y, 1, -1) + checkLine(x, y, -1, 1) >= 4
+  ) {
+    return currentPieces;
+  }
+  //console.log(checkLine(x, y, 1, 0), checkLine(x, y, -1, 0));
+  return null;
+}
